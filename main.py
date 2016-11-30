a7f5ccf61e2f7cab2bcd615b30eb4ee0970825f3
TheSwagMan <theswagman@gmx.fr>
2016-11-30 17:50:04 +0100

    Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from utils.adaptor import GuiAdapter
+from gui_utils.adaptor import GuiAdapter
 
-from utils.gui import Gui
+from gui_utils.gui import Gui
 
 if __name__ == "__main__":
     # creating Tk
--- utils/gui.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-
-class Gui(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        """
-        If you need to put a GUI layout here.
-        """
